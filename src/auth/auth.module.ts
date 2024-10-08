df09ebadd531756d9aeaa9029177d1962cf80200
harn117 <harn.16@gmail.com>
2024-10-08 07:20:27 -0500

    fix=auth-module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+
 @Module({
-  imports: [UserModule],
-  controllers: [AuthController],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      // Configura aquí tus opciones de JWT
+      secret: 'tu_secreto_jwt',
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
   providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UserService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private usersService: UserService
   ) {}
 
   async signIn(
